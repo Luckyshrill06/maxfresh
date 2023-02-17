67e7a66beb2b8baea6ca71f1ec5bc82d42ce58d4
Luckyshrill06 <102145253+Luckyshrill06@users.noreply.github.com>
2023-02-17 17:57:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+document.getElementById("signup-btn").addEventListener("click", function() {
+    window.location.href = "https://wa.me/+917999541041?text=Hello,%20I%20would%20like%20to%20place%20an%20order.";
+  });
